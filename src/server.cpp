0430a5ca2957eca633f5ea057d022b5e9c874a84
shawlynot <88781957+shawlynot@users.noreply.github.com>
2024-01-17 22:51:53 +0000

    200 ok

--- src/server.cpp
+++ src/server.cpp
@@ -49,8 +49,13 @@ int main(int argc, char **argv) {
   
   std::cout << "Waiting for a client to connect...\n";
   
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int socket_descriptor = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
+
+  std::string ok_200 { "HTTP/1.1 200 OK\r\n\r\n" };
+  int bytes = ok_200.length() * sizeof(char);
+
+  send(socket_descriptor, ok_200.data(), bytes, 0);
   
   close(server_fd);
 
