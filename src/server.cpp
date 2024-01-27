234b5fbd4d1d0e74958a835f648685fee42acf5e
shawlynot <88781957+shawlynot@users.noreply.github.com>
2024-01-27 21:07:02 +0000

    part 5

--- src/server.cpp
+++ src/server.cpp
@@ -5,6 +5,38 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sstream>
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <optional>
+
+std::vector<std::string> split_string(const std::string &request_str, const std::string &separator) {
+  std::vector<std::string> out;
+  unsigned long start_pos = 0;
+  unsigned long end_pos;
+  for (; start_pos < request_str.length(); start_pos = end_pos + separator.length()) {
+    end_pos = request_str.find(separator, start_pos);
+    if (end_pos == std::string::npos) {
+      end_pos = request_str.length();
+    }
+    out.push_back(request_str.substr(start_pos, end_pos - start_pos));
+  }
+  return out;
+}
+
+std::optional<std::string> find_header_value(const std::string &header_name, const std::string &request_str) {
+  std::vector<std::string> request_lines = split_string(request_str, "\r\n");
+  auto found = std::find_if(
+      request_lines.begin(),
+      request_lines.end(),
+      [&header_name](auto line) { return line.find(header_name) != std::string::npos; }
+  );
+  if (found == request_lines.end()) {
+    return std::nullopt;
+  }
+  return found->substr(header_name.length());
+}
+
 
 int main(int argc, char **argv) {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -42,7 +74,7 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  struct sockaddr_in client_addr{};
+  struct sockaddr_in client_addr {};
   int client_addr_len = sizeof(client_addr);
 
   std::cout << "Waiting for a client to connect...\n";
@@ -52,7 +84,7 @@ int main(int argc, char **argv) {
 
   std::cout << "Reading from socket" << std::endl;
 
-  const int len{1024};
+  const int len { 1024 };
   char input_buffer[len];
   long bytes_received = recv(socket_descriptor, input_buffer, len, 0);
   std::cout << bytes_received << std::endl;
@@ -60,26 +92,33 @@ int main(int argc, char **argv) {
   std::string request_str(input_buffer, bytes_received);
   std::string request_line = request_str.substr(0, request_str.find("\r\n"));
 
-  std::istringstream stream{request_line};
+  std::istringstream stream { request_line };
 
   std::string path;
   stream >> path >> path;
 
   std::cout << "Path: " << path << "\n";
 
-  std::string path_start = "/echo/";
-  std::size_t found = path.find(path_start);
+  std::string echo_path_start = "/echo/";
+  std::string user_agent_path = "/user-agent";
 
   std::string response;
-  if (found != std::string::npos && path.length() > path_start.length()) {
-    std::string to_echo { path.substr(path_start.length(), path.length() - path_start.length()) };
+  if (path.find(echo_path_start) != std::string::npos && path.length() > echo_path_start.length()) {
+    std::string to_echo { path.substr(echo_path_start.length(), path.length() - echo_path_start.length()) };
     std::ostringstream response_stream;
-    response_stream << "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " << to_echo.length() <<  "\r\n\r\n" << to_echo;
+    response_stream << "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " << to_echo.length()
+                    << "\r\n\r\n" << to_echo;
+    response = response_stream.str();
+  } else if (path == user_agent_path) {
+    const auto user_agent_header = find_header_value("User-Agent:", request_str);
+    std::ostringstream response_stream;
+    response_stream << "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " << user_agent_header->length()
+                    << "\r\n\r\n" << *user_agent_header;
     response = response_stream.str();
   } else if (path == "/") {
     response = "HTTP/1.1 200 OK\r\n\r\n";
   } else {
-      response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    response = "HTTP/1.1 404 Not Found\r\n\r\n";
   }
 
   unsigned long bytes = response.length();
