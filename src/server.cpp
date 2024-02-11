4f179287eeb84ddacadff7ac474eeba63540f8a1
shawlynot <88781957+shawlynot@users.noreply.github.com>
2024-02-11 13:24:44 +0000

    Tidy up project

--- src/server.cpp
+++ src/server.cpp
@@ -115,8 +115,6 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  // Since the tester restarts your program quite often, setting REUSE_PORT
-  // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
